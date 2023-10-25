180e40e522d40586e6f2d7f490019e49a6bb4d7e
leemingoo <dwmapi@naver.com>
2023-10-25 17:37:10 +0900

    refector: contentType 객체화

--- app.js
+++ app.js
@@ -1,66 +1,49 @@
 const http = require('http');
 const fs = require('fs');
+const port = 8080;
+
+const contentTypes = {
+  html: 'text/html',
+  css: 'text/css',
+  js: 'application/javascript',
+  plain: 'text/plain',
+};
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
+    let filePath = '';
+    let contentType = contentTypes.html;
+
     if (req.url === '/') {
-      fs.readFile('index.html', 'utf8', (err, data) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('서버 에러');
-        } else {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(data);
-        }
-      });
+      filePath = 'index.html';
     } else if (req.url === '/dutchPay') {
-      fs.readFile('./static/dutchPay.html', 'utf8', (err, data) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('서버 에러');
-        } else {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(data);
-        }
-      });
+      filePath = './static/dutchPay.html';
     } else if (req.url === '/static/css/indexStyle.css') {
-      fs.readFile('./static/css/indexStyle.css', 'utf8', (err, data) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('서버 에러');
-        } else {
-          res.writeHead(200, { 'Content-Type': 'text/css' });
-          res.end(data);
-        }
-      });
+      filePath = './static/css/indexStyle.css';
+      contentType = contentTypes.css;
     } else if (req.url === '/static/css/resultStyle.css') {
-      fs.readFile('./static/css/resultStyle.css', 'utf8', (err, data) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('서버 에러');
-        } else {
-          res.writeHead(200, { 'Content-Type': 'text/css' });
-          res.end(data);
-        }
-      });
+      filePath = './static/css/resultStyle.css';
+      contentType = contentTypes.css;
     } else if (req.url === '/static/js/dutch-pay.js') {
-      fs.readFile('./static/js/dutch-pay.js', 'utf8', (err, data) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('서버 에러');
-        } else {
-          res.writeHead(200, { 'Content-Type': 'Application/javascript' });
-          res.end(data);
-        }
-      });
+      filePath = './static/js/dutch-pay.js';
+      contentType = contentTypes.js;
     } else {
-      res.writeHead(404, {'Content-Type' : 'text/plain'})
-      res.end('Not Found')
+      res.writeHead(404, { 'Content-Type': contentTypes.plain });
+      return res.end('Not Found');
     }
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': contentTypes.plain });
+        return res.end('서버 에러');
+      }
+
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    });
   }
 });
 
-const port = 8080
 server.listen(port, () => {
-  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`)
-})
+  console.log(`http://localhost:${port}`);
+});
